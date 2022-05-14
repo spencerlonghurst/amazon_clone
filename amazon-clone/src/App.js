0c8e70333907c7d75a0c553d87d259123e5c7fbb
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-05-14 08:24:10 -0600

    built out the scaffolding for the header JSX

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
      <h1>hello world</h1>
+     <Header />
+
+     {/* Home */}
     </div>
   );
 }
