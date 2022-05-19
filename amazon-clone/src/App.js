f6fd10f7ed4b05e52c01ad40ecccf18535b12afd
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-05-19 12:01:59 -0600

    created login page and finished styling

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home'
 import Checkout from './Checkout';
+import Login from './Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,10 +10,10 @@ function App() {
     <Router>
 
       <div className="app">
-        <Header />
         <Routes>
-          <Route path='/checkout' element={<><Checkout/></>} />
-          <Route path='/' element={<><Home/></>} />
+          <Route path='/login' element={<><Login/></>} />
+          <Route path='/checkout' element={<><Header/><Checkout/></>} />
+          <Route path='/' element={<><Header/><Home/></>} />
         </Routes>
       </div>
 
---
+++ amazon-clone/src/Login.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Login.css';
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <div className='login'>
+      <Link to='/'>
+      <img
+        className='login_logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
+        alt='Amazon Logo'
+      />
+      </Link>
+
+      <div className='login_container'>
+        <h1>Sign in</h1>
+        <form>
+          <h5>Email</h5>
+          <input type='text'/>
+
+          <h5>Password</h5>
+          <input type='password'/>
+
+          <button className='login_signInButton'>Sign In</button>
+        </form>
+
+        <p>By signing-in you agree to the FAKE AMAZON CLONE Conditions of USE & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice</p>
+
+        <button className='login_registerButton'>Create your Amazon Account</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
